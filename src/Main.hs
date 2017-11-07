d4a3b4c5e71dffe949ebb0fe13538ef89e22c6b8
Ki Yung Ahn <kyagrd@gmail.com>
2017-11-07 11:32:17 +0800

    simple lambda syntax

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,38 @@
 
 module Main where
 
+
+import qualified Data.Map.Strict         as M
+import           SmplSyntax
+import           SmplUnif
+import           Unbound.LocallyNameless
+
+x = s2n "x"
+y = s2n "y"
+z = s2n "z"
+w = s2n "w"
+c = s2n "c"
+
+idTm = lam x (V x)
+
+t1 = lam x $ app (V y) (app (V$s2n"F") (V x))
+t2 = lam x $ app (V y) (app (V c) (appMany (V$s2n"G") [V y,V x]))
+
+sig = M.fromList [(c,Cnst), (s2n"F",Flex), (s2n"G",Flex)]
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+  print idTm
+  print . runFreshM . stepBN $ idTm `App` idTm
+  let s = runFreshM $ u (sig,[(t1,t2)],emptyMap)
+  print s
+  print . runFreshM $ expand  s (V$s2n"F")
+  print . runFreshM $ expand' s (V$s2n"F")
+  print . runFreshM $ expand  s (V$s2n"G")
+  print . runFreshM $ expand' s (V$s2n"G")
+
+{-
 import           Syntax
 import           Unbound.LocallyNameless
 import           Unif
@@ -33,3 +65,4 @@ main = do
   print . runFreshM $ expand' s (V$s2n"F")
   print . runFreshM $ expand  s (V$s2n"G")
   print . runFreshM $ expand' s (V$s2n"G")
+-}
---
+++ src/SmplSyntax.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE StandaloneDeriving        #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE UndecidableInstances      #-}
+
+module SmplSyntax    where
+
+-- import           Control.Applicative
+import           Unbound.LocallyNameless
+
+type Nm = Name Tm
+
+data Tm = V Nm
+        | Lam (Bind Nm Tm)
+        | App Tm Tm
+     deriving (Eq,Ord,Show)
+
+instance Eq (Bind Nm Tm) where (==) = aeq
+instance Ord (Bind Nm Tm) where compare = acompare
+
+$(derive [''Tm])
+
+instance Alpha Tm
+
+instance Subst Tm Tm where
+  isvar (V x) = Just (SubstName x) -- for logic variable substitution
+  isvar _     = Nothing
+
+lam x = Lam . bind x
+app = App
+
+occurs :: Alpha t => Nm -> t -> Bool
+occurs x t = x `elem` (fv t :: [Nm])
+
+---------------------------------------------------------------------------
+-- below are not used in the HOpattern-unif algorithm; just some utility
+---------------------------------------------------------------------------
+
+stepBN :: Fresh f => Tm -> f Tm
+stepBN v@(V _)           = pure v
+stepBN (Lam b)           = do (x,t) <- unbind b; Lam . bind x <$> stepBN t
+stepBN (App (Lam b) t2)  = do (x,t) <- unbind b; stepBN $ subst x t2 t
+stepBN (App t1@(V _) t2) = App <$> pure t1 <*> stepBN t2
+stepBN (App t1 t2)       = App <$> stepBN t1 <*> pure t2
+
+fixM :: (Eq t, Monad m) => (t -> m t) -> t -> m t
+fixM step t = do t' <- step t
+                 if t == t' then pure t else fixM step t'
+
+whnf :: Tm -> Bool
+whnf (V _)            = True
+whnf (App (Lam _) t2) = False
+whnf (App t1 t2)      = whnf t1 && whnf t2
+whnf (Lam _)          = True
+
+stepWH :: Fresh f => Tm -> f Tm
+stepWH v@(V _)           = pure v
+stepWH v@(Lam _)         = pure v
+stepWH (App (Lam b) t2)  = do (x,t) <- unbind b; stepBN $ subst x t2 t
+stepWH (App t1@(V _) t2) = App <$> pure t1 <*> stepBN t2
+stepWH (App t1 t2)       = App <$> stepBN t1 <*> pure t2
+
+normBeta, normWH :: Fresh m => Tm -> m Tm
+normBeta = fixM stepBN
+normWH = fixM stepWH
